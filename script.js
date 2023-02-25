a1d3830ea576d2764bd11c0ea6d49cece0326de3
nil0sgarbiero <104462098+nil0sgarbiero@users.noreply.github.com>
2023-02-25 09:58:15 -0300

    3 files modified, 2 added

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Hello!');
+function validateFields() {
+  // verifica se email não é vazio, se email é valido
+  // se email verdadeiro entao habilita recuperar senha
+  //
+  const emailValid = isEmailValid();
+  document.getElementById('recoverpass').disable = !emailValid;
+}
+function isEmailValid() {
+  const email = document.getElementById('email').value;
+  if (!email) {
+    return false;
+  }
+  return validateEmail(email);
+}
+
+function validateEmail(email) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+    return false;
+  }
+  return true;
+}
